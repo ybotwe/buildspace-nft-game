4b9e5c858d0d949baf191085752c281f9324ed0c
Yaw Botwe <ybotwe@gmail.com>
2021-12-13 14:57:55 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,186 @@
+import React, {useEffect, useState} from 'react';
+import twitterLogo from './assets/twitter-logo.svg';
+import './App.css';
+import SelectCharacter from './Components/SelectCharacter';
+import Arena from './Components/Arena';
+import { CONTRACT_ADDRESS, transformCharacterData } from './constants';
+import myEpicGame from './utils/MyEpicGame.json';
+import { ethers } from 'ethers';
+import LoadingIndicator from './Components/LoadingIndicator';
+
+// Constants
+const TWITTER_HANDLE = 'kvsv_';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [characterNFT, setCharacterNFT] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const checkNetwork = async () => {
+    try { 
+      if (window.ethereum.networkVersion !== '4') {
+        alert("Please connect to Rinkeby!");
+      }
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const {ethereum} = window;
+
+      if(!ethereum){
+        alert("Please install Metamask to run this app");
+      
+        setIsLoading(false);
+        return;
+      }else {
+
+        const accounts = await ethereum.request({method: 'eth_accounts'});
+
+        if (accounts.length !== 0){
+          const account = accounts[0];
+          setCurrentAccount(account);
+          console.log("Found an authorized account:", account);
+        } else {
+          console.log("No authorized account found");
+        }
+      }
+    } catch (error){
+      console.log(error);
+    }
+
+    setIsLoading(false);
+  };
+
+  const renderContent = () => {
+
+
+    if (isLoading) {
+      return <LoadingIndicator />;
+    }
+
+    
+      /*
+    * Scenario #1
+    */
+    if (!currentAccount) {
+      return (
+        <div className="connect-wallet-container">
+          <img
+            src="https://giffiles.alphacoders.com/118/118789.gif"
+            alt="RickxMorty Gif"
+          />
+          <button
+            className="cta-button connect-wallet-button"
+            onClick={connectWalletAction}
+          >
+            Connect Wallet To Get Started
+          </button>
+        </div>
+      );
+      /*
+      * Scenario #2
+      */
+    } else if (currentAccount && !characterNFT) {
+      return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
+    } else if (currentAccount && characterNFT) {
+      return <Arena characterNFT={characterNFT} setCharacterNFT={setCharacterNFT} />;
+    }
+  };
+
+  const connectWalletAction = async () => {
+    try {
+      const {ethereum} = window;
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+
+      
+
+      
+      const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+
+      console.log('connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error){
+      console.log(error)
+    }
+  }
+
+  /*
+   * This runs our function when the page loads.
+   */
+  useEffect(() => {
+    setIsLoading(true);
+    
+    checkIfWalletIsConnected();
+    
+  }, []);
+
+  useEffect(() => {
+    
+    
+
+    const fetchNFTMetadata = async () => {
+      console.log('Checking for Character NFT on address:', currentAccount);
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        myEpicGame.abi,
+        signer
+      );
+
+      const txn = await gameContract.checkIfUserHasNFT();
+      if(txn.name){
+        console.log('User has character NFT');
+        setCharacterNFT(transformCharacterData(txn));
+      } else {
+        console.log('No character NFT found');
+      }
+
+
+      setIsLoading(false);
+
+    }
+
+    if(currentAccount) {
+      console.log('CurrentAccount:', currentAccount);
+      checkNetwork();
+      fetchNFTMetadata();
+      
+    }
+    
+  }, [currentAccount]);
+
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">⚔️ RickxMorty Slayer ⚔️</p>
+          <p className="sub-text">Team up to protect the Metaverse!</p>
+          {renderContent()}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built with @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,17 @@
+const CONTRACT_ADDRESS = '0x8284747029C68a63fDA225c5102abCe07442bF88';
+
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+
+export { CONTRACT_ADDRESS, transformCharacterData };
